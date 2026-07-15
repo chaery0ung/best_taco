d9a3c7d8c880f66a8990dc839d6bf860228c2eab
chaery0ung <chxerxy@naver.com>
2026-07-15 09:02:11 +0900

    Revert "Replace taco landing page with Green Flag fraud-detection demo"
    This reverts commit 63654c7fec2a338fa84682b8065b8577e2e0dffe.

--- background.js
+++
@@ -1,7 +0,0 @@
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.get({ enabled: true }, (res) => {
-    if (res.enabled === undefined) {
-      chrome.storage.sync.set({ enabled: true });
-    }
-  });
-});
